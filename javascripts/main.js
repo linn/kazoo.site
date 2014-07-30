d0ec5d8f869ec317be4241af723f028d115920cd
chrischeung <chris.cheung@linn.co.uk>
2014-07-30 16:38:40 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+<script type="text/javascript">
+         function calcHeight()
+         {
+         //find the height of the internal page
+         var the_height=
+         document.getElementById('the_iframe').contentWindow.
+         document.body.scrollHeight;
+
+         //change the height of the iframe
+         document.getElementById('the_iframe').height=
+         the_height;
+         }
+</script>
